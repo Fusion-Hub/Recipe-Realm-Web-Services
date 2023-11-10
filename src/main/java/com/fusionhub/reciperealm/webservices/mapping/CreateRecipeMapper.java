555777ab84d77915779e4e6043de2e157ad2e6c0
Rodrigo Lopez <64718167+Rodrigoalt15@users.noreply.github.com>
2023-11-09 22:17:05 -0500

    Update CreateRecipeMapper.java

--- src/main/java/com/fusionhub/reciperealm/webservices/mapping/CreateRecipeMapper.java
+++ src/main/java/com/fusionhub/reciperealm/webservices/mapping/CreateRecipeMapper.java
@@ -2,10 +2,12 @@ package com.fusionhub.reciperealm.webservices.mapping;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import com.fusionhub.reciperealm.webservices.dto.CreateRecipeDto;
 import com.fusionhub.reciperealm.webservices.models.Recipe;
 
+@Component
 public class CreateRecipeMapper {
 
     @Autowired
